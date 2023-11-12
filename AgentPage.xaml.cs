92d2b26c68bc147895604091b61adb3accbe6083
Ayupov Bulat <b.aiupov@yandex.ru>
2023-11-12 22:15:58 +0500

    12 лаба

--- AgentPage.xaml.cs
+++ AgentPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,9 +19,14 @@ namespace Ayupov_Glazki
     /// <summary>
     /// Логика взаимодействия для AgentPage.xaml
     /// </summary>
+   
     public partial class AgentPage : Page
     {
-
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
         public AgentPage()
         {
             InitializeComponent();
@@ -32,7 +38,86 @@ namespace Ayupov_Glazki
 
             UpdateAgents();
         }
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords=TableList.Count;
 
+            if(CountRecords %10>0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            Boolean Ifupdate = true;
+
+            int min;
+            if(selectedPage.HasValue) 
+            {
+                if(selectedPage>=0 && selectedPage<=CountPage) 
+                {
+                    CurrentPage=(int)selectedPage;
+                    min=CurrentPage *10+10<CountRecords ? CurrentPage *10+10 : CountRecords;
+                    for(int i=CurrentPage*10;i<min;i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch(direction)
+                {
+                    case 1:
+                        if(CurrentPage>0)
+                        {
+                            CurrentPage--;
+                            min=CurrentPage*10+10<CountRecords ? CurrentPage*10+10 : CountRecords;
+                            for(int i=CurrentPage*10; i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                        case 2:
+                        if(CurrentPage<CountPage-1)
+                        {
+                            CurrentPage++;
+                            min=CurrentPage*10+10<CountRecords ? CurrentPage *10+10 : CountRecords;
+                            for(int i=CurrentPage*10;i<min;i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }    
+                        else
+                        {
+                            Ifupdate=false;
+                        }
+                        break;
+                }
+            }
+            if(Ifupdate)
+            {
+                PageListBox.Items.Clear();
+                for(int i=1;i<=CountPage;i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex= CurrentPage;
+                min=CurrentPage*10+10<CountRecords? CurrentPage*10+10 :CountRecords;
+                TBCount.Text=min.ToString();
+                TBAllRecords.Text=" из " +CountRecords.ToString();
+                AgentListView.ItemsSource = CurrentPageList;
+                AgentListView.Items.Refresh();
+            }
+        }
         private void UpdateAgents()
         {
             var currentAgent = AyupovGlazkiEntities.GetContext().Agent.ToList();
@@ -88,7 +173,8 @@ namespace Ayupov_Glazki
                 currentAgent = currentAgent.Where(p => p.AgentTypeID == 6).ToList();
             }
             AgentListView.ItemsSource = currentAgent;
-
+            TableList = currentAgent;
+            ChangePage(0,0);
 
         }
 
@@ -110,6 +196,21 @@ namespace Ayupov_Glazki
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             UpdateAgents();
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0,Convert.ToInt32(PageListBox.SelectedItem.ToString())-1);
         }
     }
 }
